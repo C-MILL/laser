e3422317d7716c141797b1efecff22a912e57bcc
Christof <christof.mueller@sipbb.ch>
2024-05-26 18:30:22 +0200

    serial tests

---
+++ arduino.py
@@ -0,0 +1,14 @@
+import serial
+import time
+if __name__ == '__main__':
+    # if connected via serial Pin(RX, TX)
+    ser = serial.Serial('/dev/ttyS0', 115200, timeout=1) #9600 is baud rate(must be same with that of NodeMCU)
+    ser.flush()
+while True:
+        string = input("enter string:") #input from user
+        string = string +"\n" #"\n" for line seperation
+        string = string.encode('utf_8')
+        ser.write(string)
+        line = ser.readline().decode('utf-8').rstrip()
+        print("received: ",line)
+        time.sleep(1) #delay of 1 second
