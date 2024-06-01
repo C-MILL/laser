e7e9e6213a2fd15483712a469a2464b72af2f619
chrigu <mueller.christof@outlook.com>
2024-06-01 17:55:01 +0200

    some stuff

--- serial_test/Laser-CNC.ino
+++ serial_test/Laser-CNC.ino
@@ -1,101 +1,131 @@
-int Pos_x = 0;
-int Pos_y = 0;
+// int PUL = 5; //define Pulse pin
+// int DIR = 4; //define Direction pin
+// int ENA = 34; //define Enable Pin
+// void setup() {
+//   pinMode (PUL, OUTPUT);
+//   pinMode (DIR, OUTPUT);
+//   pinMode (ENA, OUTPUT);
+// }
+
+// void loop() {
+//   for (int i = 0; i < 6400; i++) // Forward 5000 steps
+//   {
+//     digitalWrite(DIR, LOW);
+//     digitalWrite(ENA, HIGH);
+//     digitalWrite(PUL, HIGH);
+//     delayMicroseconds(50);
+//     digitalWrite(PUL, LOW);
+//     delayMicroseconds(50);
+//   }
+//   for (int i = 0; i < 6400; i++) // Backward 5000 steps
+//   {
+//     digitalWrite(DIR, HIGH);
+//     digitalWrite(ENA, HIGH);
+//     digitalWrite(PUL, HIGH);
+//     delayMicroseconds(50);
+//     digitalWrite(PUL, LOW);
+//     delayMicroseconds(50);
+//   }
+// }
+
+// int Pos_x = 0;
+// int Pos_y = 0;
 
 
-void Motor_1_L(int Pos_x) {
-  digitalWrite(4, HIGH);
-  digitalWrite(13, LOW);
+// void Motor_1_L(int Pos_x) {
+//   digitalWrite(4, HIGH);
+//   digitalWrite(13, LOW);
 
-  for (Pos_x = 0; Pos_x < 1000; Pos_x++)
+//   for (Pos_x = 0; Pos_x < 1000; Pos_x++)
 
-  {
-    digitalWrite(5, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(5, LOW);
-    delayMicroseconds(1000);
-  }
-}
+//   {
+//     digitalWrite(5, HIGH);
+//     delayMicroseconds(1000);
+//     digitalWrite(5, LOW);
+//     delayMicroseconds(1000);
+//   }
+// }
 
-void Motor_1_R(int Pos_x) {
-  digitalWrite(13, HIGH);
-  digitalWrite(4, LOW);
+// void Motor_1_R(int Pos_x) {
+//   digitalWrite(13, HIGH);
+//   digitalWrite(4, LOW);
 
-  for (Pos_x = 0; Pos_x < 1000; Pos_x++) {
-    digitalWrite(5, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(5, LOW);
-    delayMicroseconds(1000);
-  }
-}
+//   for (Pos_x = 0; Pos_x < 1000; Pos_x++) {
+//     digitalWrite(5, HIGH);
+//     delayMicroseconds(1000);
+//     digitalWrite(5, LOW);
+//     delayMicroseconds(1000);
+//   }
+// }
 
-void Motor_2_L(int Pos_x) {
-  digitalWrite(16, HIGH);
-  digitalWrite(2, LOW);
+// void Motor_2_L(int Pos_x) {
+//   digitalWrite(16, HIGH);
+//   digitalWrite(2, LOW);
 
-  for (Pos_x = 0; Pos_x < 1000; Pos_x++) {
-    digitalWrite(18, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(18, LOW);
-    delayMicroseconds(1000);
-  }
-}
+//   for (Pos_x = 0; Pos_x < 1000; Pos_x++) {
+//     digitalWrite(18, HIGH);
+//     delayMicroseconds(1000);
+//     digitalWrite(18, LOW);
+//     delayMicroseconds(1000);
+//   }
+// }
 
-void Motor_2_R(int Pos_x) {
-  digitalWrite(2, HIGH);
-  digitalWrite(16, LOW);
+// void Motor_2_R(int Pos_x) {
+//   digitalWrite(2, HIGH);
+//   digitalWrite(16, LOW);
 
-  for (Pos_x = 0; Pos_x < 1000; Pos_x++) {
-    digitalWrite(18, HIGH);
-    delayMicroseconds(100);
-    digitalWrite(18, LOW);
-    delayMicroseconds(100);
-  }
-}
+//   for (Pos_x = 0; Pos_x < 1000; Pos_x++) {
+//     digitalWrite(18, HIGH);
+//     delayMicroseconds(100);
+//     digitalWrite(18, LOW);
+//     delayMicroseconds(100);
+//   }
+// }
 
 
-void setup() {
-  pinMode(34, OUTPUT);  //Enable Stepp 1
-  pinMode(5, OUTPUT);   //Puls Stepp 1
-  pinMode(4, OUTPUT);   //Direction Stepp 1
-  pinMode(13, OUTPUT);  //Direction - Stepp 1
+// void setup() {
+//   pinMode(34, OUTPUT);  //Enable Stepp 1
+//   pinMode(5, OUTPUT);   //Puls Stepp 1
+//   pinMode(4, OUTPUT);   //Direction Stepp 1
+//   pinMode(13, OUTPUT);  //Direction - Stepp 1
 
-  pinMode(35, OUTPUT);  //Enable Stepp 2
-  pinMode(18, OUTPUT);  //Puls Stepp 2
-  pinMode(16, OUTPUT);  //Direction Stepp 2
-  pinMode(2, OUTPUT);   //Direction - Stepp 2
+//   pinMode(35, OUTPUT);  //Enable Stepp 2
+//   pinMode(18, OUTPUT);  //Puls Stepp 2
+//   pinMode(16, OUTPUT);  //Direction Stepp 2
+//   pinMode(2, OUTPUT);   //Direction - Stepp 2
 
-  digitalWrite(34, LOW);
-  digitalWrite(35, LOW);
+//   digitalWrite(34, LOW);
+//   digitalWrite(35, LOW);
 
-  Serial.begin(115200);  // Ensure the baud rate matches
-  Serial.println("ESP32 ready");
-}
+//   Serial.begin(115200);  // Ensure the baud rate matches
+//   Serial.println("ESP32 ready");
+// }
 
-void loop() {
+// void loop() {
 
-  //Motor_2_L(Pos_x);
-  //Motor_2_R(Pos_x);
-  Motor_2_L(Pos_x);
-  //Motor_2_R(Pos_x);
-}
+//   //Motor_2_L(Pos_x);
+//   //Motor_2_R(Pos_x);
+//   Motor_2_L(Pos_x);
+//   //Motor_2_R(Pos_x);
+// }
 
-//init
-//baud rate = 115200
-//Arduino datenempfangsbereit
-//Pi sendet Daten
-//loop Arduino
-//{
-//Daten empfangen
-//Pi in Pause setzen
-//prüfen, ob keine Koordinate mehr verfügbar, fortführen wenn Koordinate vorhanden
-//Daten zu Position umwandeln
-//Stepp mit for an Position senden und überprüfen
-//Laser einschalten
-//delay
-//Laser ausschalten
-//Arduino datenempfangsbereit
-//Pi fortsetzen
-//Pi sendet Daten
-//}
+// //init
+// //baud rate = 115200
+// //Arduino datenempfangsbereit
+// //Pi sendet Daten
+// //loop Arduino
+// //{
+// //Daten empfangen
+// //Pi in Pause setzen
+// //prüfen, ob keine Koordinate mehr verfügbar, fortführen wenn Koordinate vorhanden
+// //Daten zu Position umwandeln
+// //Stepp mit for an Position senden und überprüfen
+// //Laser einschalten
+// //delay
+// //Laser ausschalten
+// //Arduino datenempfangsbereit
+// //Pi fortsetzen
+// //Pi sendet Daten
+// //}
 
-//Func Motor 1
+// //Func Motor 1
---
+++ serial_test/accstepper.ino
@@ -0,0 +1,159 @@
+#include <AccelStepper.h>
+
+long receivedMMdistance = 0; // distance in mm from the computer
+long receivedDelay = 0; // delay between two steps, received from the computer
+long receivedAcceleration = 0; // acceleration value from computer
+char receivedCommand; // character for commands
+/* s = Start (CCW) // needs steps and speed values
+ * o = open (CCW) // needs steps and speed values
+ * c = close (CW) // needs steps and speed values
+ * a = set acceleration // needs acceleration value
+ * n = stop right now! // just the 'n' is needed
+ */
+
+bool newData, runallowed = false; // booleans for new data from serial, and runallowed flag
+
+// direction Digital 9 (CCW), pulses Digital 8 (CLK)
+AccelStepper stepper1(1, 5, 4);
+AccelStepper stepper2(1, 18, 16);
+
+void setup()
+{
+  Serial.begin(9600); // define baud rate
+  Serial.println("Testing Accelstepper"); // print a message
+
+  // setting up some default values for maximum speed and maximum acceleration
+  stepper1.setMaxSpeed(2000); // SPEED = Steps / second
+  stepper1.setAcceleration(1000); // ACCELERATION = Steps /(second)^2
+  stepper2.setMaxSpeed(2000); // SPEED = Steps / second
+  stepper2.setAcceleration(1000); // ACCELERATION = Steps /(second)^2
+
+  stepper1.disableOutputs(); // disable outputs, so the motor is not getting warm (no current)
+  stepper2.disableOutputs(); // disable outputs, so the motor is not getting warm (no current)
+}
+
+void loop()
+{
+  checkSerial(); // check serial port for new commands
+  continuousRun(); // method to handle the motors
+}
+
+void continuousRun() // method for the motors
+{
+  if (runallowed == true)
+  {
+    if (abs(stepper1.currentPosition()) < receivedMMdistance || abs(stepper2.currentPosition()) < receivedMMdistance) // abs() is needed because of the '<'
+    {
+      stepper1.enableOutputs(); // enable pins
+      stepper2.enableOutputs(); // enable pins
+      stepper1.run(); // step the motor (this will step the motor by 1 step at each loop)
+      stepper2.run(); // step the motor (this will step the motor by 1 step at each loop)
+    }
+    else // program enters this part if the required distance is completed
+    {
+      runallowed = false; // disable running -> the program will not try to enter this if-else anymore
+      stepper1.disableOutputs(); // disable power
+      stepper2.disableOutputs(); // disable power
+      Serial.print("POS1: ");
+      Serial.println(stepper1.currentPosition()); // print pos -> this will show you the latest relative number of steps
+      Serial.print("POS2: ");
+      Serial.println(stepper2.currentPosition()); // print pos -> this will show you the latest relative number of steps
+      stepper1.setCurrentPosition(0); // reset the position to zero
+      stepper2.setCurrentPosition(0); // reset the position to zero
+      Serial.print("POS1: ");
+      Serial.println(stepper1.currentPosition()); // print pos -> this will show you the latest relative number of steps; we check here if it is zero for real
+      Serial.print("POS2: ");
+      Serial.println(stepper2.currentPosition()); // print pos -> this will show you the latest relative number of steps; we check here if it is zero for real
+    }
+  }
+  else // program enters this part if the runallowed is FALSE, we do not do anything
+  {
+    return;
+  }
+}
+
+void checkSerial() // method for receiving the commands
+{  
+  if (Serial.available() > 0) // if something comes
+  {
+    receivedCommand = Serial.read(); // this will read the command character
+    newData = true; // this creates a flag
+  }
+
+  if (newData == true) // if we received something (see above)
+  {
+    // START - MEASURE
+    if (receivedCommand == 's') // this is the measure part
+    {
+      runallowed = true; // allow running
+      receivedMMdistance = Serial.parseFloat(); // value for the steps
+      receivedDelay = Serial.parseFloat(); // value for the speed
+
+      Serial.print(receivedMMdistance); // print the values for checking
+      Serial.print(receivedDelay);
+      Serial.println(" Measure"); // print the action
+      stepper1.setMaxSpeed(receivedDelay); // set speed
+      stepper1.move(receivedMMdistance); // set distance
+      stepper2.setMaxSpeed(receivedDelay); // set speed
+      stepper2.move(receivedMMdistance); // set distance
+    }
+    // START - OPEN
+    if (receivedCommand == 'o') // OPENING
+    {
+      runallowed = true; // allow running
+      receivedMMdistance = Serial.parseFloat(); // value for the steps
+      receivedDelay = Serial.parseFloat(); // value for the speed
+
+      Serial.print(receivedMMdistance); // print the values for checking
+      Serial.print(receivedDelay);
+      Serial.println(" OPEN"); // print the action
+      stepper1.setMaxSpeed(receivedDelay); // set speed
+      stepper1.move(receivedMMdistance); // set distance
+      stepper2.setMaxSpeed(receivedDelay); // set speed
+      stepper2.move(receivedMMdistance); // set distance
+    }
+
+    // START - CLOSE
+    if (receivedCommand == 'c') // CLOSING - Rotates the motor in the opposite direction as opening
+    {
+      runallowed = true; // allow running
+      receivedMMdistance = Serial.parseFloat(); // value for the steps
+      receivedDelay = Serial.parseFloat(); // value for the speed
+
+      Serial.print(receivedMMdistance);  // print the values for checking
+      Serial.print(receivedDelay);
+      Serial.println(" CLOSE"); // print action
+      stepper1.setMaxSpeed(receivedDelay); // set speed
+      stepper1.move(-1 * receivedMMdistance); // set distance - negative value flips the direction
+      stepper2.setMaxSpeed(receivedDelay); // set speed
+      stepper2.move(-1 * receivedMMdistance); // set distance - negative value flips the direction
+    }
+
+    // STOP - STOP
+    if (receivedCommand == 'n') // immediately stops the motor
+    {
+      runallowed = false; // disable running
+      stepper1.setCurrentPosition(0); // reset position
+      stepper2.setCurrentPosition(0); // reset position
+      Serial.println(" STOP"); // print action
+      stepper1.stop(); // stop motor
+      stepper1.disableOutputs(); // disable power
+      stepper2.stop(); // stop motor
+      stepper2.disableOutputs(); // disable power
+    }
+
+    // SET ACCELERATION
+    if (receivedCommand == 'a') // Setting up a new acceleration value
+    {
+      runallowed = false; // we still keep running disabled, since we just update a variable
+      receivedAcceleration = Serial.parseFloat(); // receive the acceleration from serial
+
+      stepper1.setAcceleration(receivedAcceleration); // update the value of the variable
+      stepper2.setAcceleration(receivedAcceleration); // update the value of the variable
+
+      Serial.println(" ACC Updated"); // confirm update by message
+    }
+  }
+  // after we went through the above tasks, newData becomes false again, so we are ready to receive new commands again.
+  newData = false;
+}
